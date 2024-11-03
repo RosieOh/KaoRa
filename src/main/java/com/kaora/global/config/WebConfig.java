b0a2a09ce47b63f1c267fd1db6ae50ddb324407d
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-03 18:26:21 +0900

    FEAT : Web Application 설정 로직 추가 및 ModelMapper 빈등록

---
+++ src/main/java/com/kaora/global/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.kaora.global.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry reg) {
+        reg.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        reg.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper md = new ModelMapper();
+        md.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return md;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        WebMvcConfigurer.super.configureMessageConverters(converters);
+    }
+}
