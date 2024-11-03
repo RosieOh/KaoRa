d8156db3c73dc627c0d37dd0c21e42065c51fc7d
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-03 20:53:41 +0900

    FEAT : Cors Error 방지 설정 로직 추가

--- src/main/java/com/kaora/global/config/CorsConfig.java
+++ src/main/java/com/kaora/global/config/CorsConfig.java
@@ -1,4 +1,23 @@
 package com.kaora.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration cf = new CorsConfiguration();
+        cf.setAllowCredentials(true);
+        cf.addAllowedOrigin("*");
+        cf.addAllowedHeader("*");
+        cf.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", cf);
+        return new CorsFilter(source);
+    }
 }
