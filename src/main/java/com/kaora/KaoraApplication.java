85ff246dfe0b8dbb4d1235165d5e0460c3783c14
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-03 17:46:42 +0900

    BUILD : 모두의 카페 리뷰를 기록하는 저장소, 카오라 서버 빌드

---
+++ src/main/java/com/kaora/KaoraApplication.java
@@ -0,0 +1,13 @@
+package com.kaora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaora/KaoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
