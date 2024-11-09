5523cf7034429f1cec7502e806f83a4c51f05e03
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-09 19:30:22 +0900

    FEAT : 기본 게시판 서비스 레이어 추가

--- src/main/java/com/kaora/domain/board/service/BoardService.java
+++ src/main/java/com/kaora/domain/board/service/BoardService.java
@@ -1,4 +1,25 @@
 package com.kaora.domain.board.service;
 
-public class BoardService {
+import com.kaora.domain.board.dto.BoardDTO;
+import com.kaora.domain.board.entity.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    public BoardDTO findById(Long id);
+
+    public List<BoardDTO> findAll(BoardDTO bd);
+
+    public List<Board> list();
+
+    public void register(BoardDTO bd);
+
+    public void modify(BoardDTO bd);
+
+    public void remove(Long id);
+
+    public BoardDTO getBoard(Long id);
+
+    public List<BoardDTO> findByBoardType(String boardType);
 }
