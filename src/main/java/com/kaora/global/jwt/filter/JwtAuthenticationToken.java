534ad13fe7f5e7ed4516966a0ac3f098494888e6
RosieOh <20172207@gm.hannam.ac.kr>
2024-11-09 18:13:14 +0900

    FEAT : 유효 토큰 보유 시 사용자를 검증하는 인증 필터 로직 구현

---
+++ src/main/java/com/kaora/global/jwt/filter/JwtAuthenticationToken.java
@@ -0,0 +1,30 @@
+package com.kaora.global.jwt.filter;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Object principal;
+    private final String credentials;
+
+    public JwtAuthenticationToken(Object principal, String credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return principal;
+    }
+}
